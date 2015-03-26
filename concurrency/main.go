88447768fb82b961a55b1c6d40ce2b44eb38294c
Kenneth Feng <kennethzfeng@gmail.com>
2015-03-25 22:43:11 -0400

    Replace done channel with sync.WaitGroup

--- concurrency/main.go
+++ concurrency/main.go
@@ -9,25 +9,25 @@ import (
 
 var counter = 0
 var mutex = &sync.Mutex{}
-var done = make(chan bool)
+var wg = &sync.WaitGroup{}
 
 func main() {
 	// Set up the Seed for rand.Intn
 	rand.Seed(time.Now().Unix())
 
+	wg.Add(10)
+
 	for i := 0; i < 10; i++ {
 		go func(i int) {
+			defer wg.Done()
+
 			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 			fmt.Printf("Counter %d: %d\n", i, counter)
 			mutex.Lock()
 			counter += 1
 			mutex.Unlock()
-
-			done <- true
 		}(i)
 	}
 
-	for i := 0; i < 10; i++ {
-		<-done
-	}
+	wg.Wait()
 }
