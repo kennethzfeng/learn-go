26d11056213233974aa2fc03c7b7d9700602fa5a
Kenneth Feng <kennethzfeng@gmail.com>
2015-03-25 22:10:11 -0400

    Add Demo use for sync.Mutex

--- concurrency/main.go
+++ concurrency/main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 var counter = 0
+var mutex = &sync.Mutex{}
 
 func main() {
+	// Set up the Seed for rand.Intn
+	rand.Seed(time.Now().Unix())
+
 	for i := 0; i < 10; i++ {
 		go func(i int) {
-			fmt.Printf("Counter %d: %d\n", i, rand.Intn(5))
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+			fmt.Printf("Counter %d: %d\n", i, counter)
+			mutex.Lock()
+			counter += 1
+			mutex.Unlock()
 		}(i)
 	}
 
-	time.Sleep(10)
+	time.Sleep(6 * time.Second)
 }
